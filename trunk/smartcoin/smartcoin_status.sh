a044a3130fb40e06d2de47b43e0d0ac60559ab87
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-18 19:57:55 +0000

    boo

--- trunk/smartcoin/smartcoin_status.sh
+++ trunk/smartcoin/smartcoin_status.sh
@@ -8,7 +8,7 @@
 ShowStatus() {
 	status=""
 	UseDB "smartcoin"
-	Q="Select device from card WHERE NOT disabled ORDER BY device ASC";
+	Q="Select device from card WHERE disabled=0 ORDER BY device ASC";
 	R=$(RunSQL "$Q")
 
 
