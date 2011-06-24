72cdc2f5ccf7f958fe7a19f6f2a671669da1b3b5
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-24 23:30:13 +0000

    still wo rkign on it...

--- smartcoin/smartcoin_control.sh
+++ smartcoin/smartcoin_control.sh
@@ -592,7 +592,7 @@ Add_Workers()
 	echo ""
 
 	E="Would you like this worker to be available to the automatic profile? (y)es or (n)o?"
-	GetYesNoSelection workerAllow "$E" "1"
+	GetYesNoSelection workerAllow "$E" 1
 
 	echo "Adding Worker..."
         Q="INSERT INTO worker (fk_pool, name, user, pass, priority, auto_allow, disabled) VALUES ($thisPool,'$workerName,'$userName','$password',$workerPriority,$workerAllow,0);"
