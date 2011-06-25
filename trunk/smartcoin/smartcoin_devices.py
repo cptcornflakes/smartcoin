9e8b817d76379baaa07c3421ac691f7f6acf15e7
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-25 00:38:16 +0000

    fixed a stupid cpu/gpu bug

--- trunk/smartcoin/smartcoin_devices.py
+++ trunk/smartcoin/smartcoin_devices.py
@@ -7,7 +7,7 @@ for i in xrange(len(devices)):
 	if "CPU" in devices[i].name:
 		print '%d\tCPU[%d]\t1\tcpu' % (i,i) 
 	else:
-		print '%d\tGPU[%d]\t0\tcpu' % (i,i)
+		print '%d\tGPU[%d]\t0\tgpu' % (i,i)
 
 
 
