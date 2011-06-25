064f30e8f729c8cb130a19114e92c7644eb0badb
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-25 16:24:31 +0000

    Removed debugging stufff. Fixed bug where you would endlessly loop while adding a profile.

--- trunk/smartcoin/smartcoin_control.sh
+++ trunk/smartcoin/smartcoin_control.sh
@@ -125,7 +125,6 @@ GetYesNoSelection() {
 			echo "Invalid response!"                                
 		fi                                                              
 	done    
-		echo "Got response: $resp"
 	
 	eval $_retyn="'$resp'"
 }
@@ -800,16 +799,9 @@ Add_Profile()
 		E="$E Would you like to continue adding instances to this profile? (y)es or (n)o?"
 		GetYesNoSelection resp "$E"
 
-		echo "resp = $resp"
-
 		if [[ "$resp" == "0" ]]; then
-			echo "resp was zero, finish up!"
 			finished="1"
-		else
-			echo "resp was non-zero"
 		fi
-		echo "Any key to continue..."
-		read blah
 	done	
 	clear
 	ShowHeader
