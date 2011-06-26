7ff84944e6865cb0e2a737bab73757b8252acde5
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-26 17:10:40 +0000

    anther update.... set query bug

--- branches/beta1/smartcoin/smartcoin_install.sh
+++ branches/beta1/smartcoin/smartcoin_install.sh
@@ -150,7 +150,7 @@ fi
 # Set the default miner
 
 # TODO: I should add more logic to determining a default.... Perhaps ask the user which one?
-Q="UPDATE miner SET (default_miner) VALUES ('1') WHERE pk_miner=1;"
+Q="UPDATE miner SET default_miner='1' WHERE pk_miner=1;"
 RunSQL "$Q"
 
 # Set the current profile! 
