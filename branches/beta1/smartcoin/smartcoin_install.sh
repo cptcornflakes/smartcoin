fcbbaa6deb1ec4343422e25deba73861318aa821
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-26 16:17:42 +0000

    New rewrite to support sqlite instead of MySQL - most users don't know how to properly set up MySQL, which is going to turn into a support nightmare
    Also, added the starts of a pre-install check that will exit the installer if certain
    criteria isn't met

--- branches/beta1/smartcoin/smartcoin_install.sh
+++ branches/beta1/smartcoin/smartcoin_install.sh
@@ -5,9 +5,26 @@
 # [ ] Check on better way to interactively install packages...
 
 
+
+preInstallCheck() {
+	# Just a start at detecting things that should be done PRIOR to trying to run smartcoin
+
+	ready=""
+	ready=`echo $LD_LIBRARY_PATH | grep AMD-APP-SDK`
+	if [[ "$ready" == "" ]]; then
+		echo "It appears that the AMD APP SDK is not in your LD_LIBRARY_PATH"
+		echo "This is required for smartcoin to work properly!"
+		echo "Please add the AMD APP SDK path to LD_LIBRARY_PATH and try again."
+	fi
+}
+
+
 . $HOME/smartcoin/smartcoin_ops.sh
 
 clear
+
+preInstallCheck
+
 
 echo "SmartCoin requires root permissions to install dependencies, create SymLinks and set up the database."
 echo "You will be prompted for  your password when needed."
@@ -30,25 +47,11 @@ echo ""
 
 # Install dependencies
 echo  "Installing dependencies, please be patient..."
-sudo apt-get install -f  -y sysstat mysql-server mysql-client openssh-server #2> /dev/null
+sudo apt-get install -f  -y sysstat sqlite3 openssh-server #2> /dev/null
 echo "done."
 echo ""
 
-# Set up MySQL
-UseDB "smartcoin"
-echo "Configuring MySQL..."
-sudo mysql -A -N -p -e "CREATE DATABASE smartcoin;"
-sudo mysql -A -N -p -e "CREATE USER 'smartcoin'@'localhost'  IDENTIFIED BY 'smartcoin';" 
-sudo mysql -A -N -p -e "GRANT ALL PRIVILEGES ON smartcoin.* TO 'smartcoin'@'localhost' IDENTIFIED BY 'smartcoin';" 
-echo "done."
-echo ""
-echo "Importing database schema..."
-if [[ -f $HOME/smartcoin/smartcoin_schema.sql ]]; then
-     sudo mysql smartcoin < $HOME/smartcoin/smartcoin_schema.sql 2> /dev/null
-fi
 
-echo "done."
-echo ""
 
 # Set up the local machine
 Q="INSERT INTO machine (name,server,ssh_port,username,auto_allow,disabled) VALUES ('localhost','127.0.0.1',22,'$USER',1,0);"
@@ -56,47 +59,47 @@ RunSQL "$Q"
 
 
 # Populate the database with default pools
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('DeepBit','deepbit.net',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('DeepBit','deepbit.net',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('Bitcoin.cz (slush)','mining.bitcoin.cz',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('Bitcoin.cz (slush)','mining.bitcoin.cz',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('BTCGuild','btcguild.com',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('BTCGuild','btcguild.com',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('BTCMine','btcmine.com',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('BTCMine','btcmine.com',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('Bitcoins.lc','bitcoins.lc',NULL,8080,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('Bitcoins.lc','bitcoins.lc',NULL,8080,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('SwePool','swepool.net',NULL,8337,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('SwePool','swepool.net',NULL,8337,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('Continuum','continuumpool.com',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('Continuum','continuumpool.com',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('MineCo','mineco.in',NULL,3000,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('MineCo','mineco.in',NULL,3000,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('Eligius','mining.eligius.st',NULL,8337,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('Eligius','mining.eligius.st',NULL,8337,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('CoinMiner','173.0.52.116',NULL,8347,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('CoinMiner','173.0.52.116',NULL,8347,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('ZABitcoin','mine.zabitcoin.co.za',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('ZABitcoin','mine.zabitcoin.co.za',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('BitClockers','pool.bitclockers.com',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('BitClockers','pool.bitclockers.com',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('MtRed','mtred.com',NULL,8337,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('MtRed','mtred.com',NULL,8337,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('SimpleCoin','simplecoin.us',NULL,8337,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('SimpleCoin','simplecoin.us',NULL,8337,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('Ozco','ozco.in',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('Ozco','ozco.in',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('EclipseMC','us.eclipsemc.com',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('EclipseMC','us.eclipsemc.com',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('BitP','pool.bitp.it',NULL,8334,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('BitP','pool.bitp.it',NULL,8334,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('BitcoinPool','bitcoinpool.com',NULL,8334,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('BitcoinPool','bitcoinpool.com',NULL,8334,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('EcoCoin','ecocoin.org',NULL,8332,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('EcoCoin','ecocoin.org',NULL,8332,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('BitLottoPool','bitcoinpool.com',NULL,8337,60,1,0);"
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('BitLottoPool','bitcoinpool.com',NULL,8337,60,1,0);"
 R=$(RunSQL "$Q")
-Q="INSERT IGNORE INTO pool (name,server,alternateServer,port,timeout,auto_allow,disabled) VALUES ('X8S','pit.x8s.de',NULL,8337,60,1,0);"                              
+Q="INSERT IGNORE INTO pool (name,server,alternate_server,port,timeout,auto_allow,disabled) VALUES ('X8S','pit.x8s.de',NULL,8337,60,1,0);"                              
 R=$(RunSQL "$Q")
 
 # Autodetect cards
