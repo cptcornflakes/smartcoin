7df7d158bdc8dfc638f278ee76038a3cc84ec91c
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-06-30 08:25:12 +0000

    rework aticonfig call to get usage %

--- branches/beta1/smartcoin/smartcoin_status.sh
+++ branches/beta1/smartcoin/smartcoin_status.sh
@@ -100,8 +100,8 @@ ShowStatus() {
 			sleep 0.2 # aticonfig seems to get upset sometimes if it is called very quickly in succession
 		        temperature=$(aticonfig --adapter=$deviceID --odgt | awk '/Temperature/ { print $5 }';)
 			sleep 0.2 # aticonfig seems to get upset sometimes if it is called very quickly in succession
-			usage=$(aticonfig --pplib-cmd "get activity" | awk '/Activity\:/ { print $2 }')
-			status=$status"$deviceName: Temp: $temperature load: $usage\n"
+      usage=$(aticonfig --adapter=$deviceID --odgc | awk '/GPU\ load/ { print $4 }';)
+			status=$status"$deviceName: Temp: $temperature load: $usage%\n"
 		fi
 	done
 	cpu=`iostat | awk '{if(NR==4) {print "CPU Load : " $1 "%"}}'`
