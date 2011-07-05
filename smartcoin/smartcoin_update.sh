f4c1206a97bfd28df9d3854b7c98106a32d4c7f3
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-05 18:20:12 +0000

    this didn't get merged for some reason

--- smartcoin/smartcoin_update.sh
+++ smartcoin/smartcoin_update.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-. $HOME/smartcoin/smartcoin_ops.sh
+if [[ $( dirname "$0" ) == "/usr/bin" ]]; then
+	CUR_LOCATION=$(pwd)
+else
+	CUR_LOCATION="$( cd "$( dirname "$0" )" && pwd )"
+fi
+. $CUR_LOCATION/smartcoin_ops.sh
 experimental_update=$1
 
 Log "Preparing to do an Update..." 1
-svn_rev_start=`svn info $HOME/smartcoin/ | grep "^Revision" | awk '{print $2}'`
-svn_current_repo=`svn info $HOME/smartcoin/ | grep "^URL" | awk '{print $2}'`
+svn_rev_start=`svn info $CUR_LOCATION/ | grep "^Revision" | awk '{print $2}'`
+svn_current_repo=`svn info $CUR_LOCATION/ | grep "^URL" | awk '{print $2}'`
 svn_rev_end=`svn info $svn_current_repo | grep "^Revision" | awk '{print $2}'`
-safe_update=1 #TODO: UNCOMMENT THIS WHEN READY FOR STABLE UPDATES!`svn diff $HOME/smartcoin/ -r $svn_rev_start:$svn_rev_end update.ver`
+safe_update=1 #TODO: UNCOMMENT THIS WHEN READY FOR STABLE UPDATES!`svn diff $CUR_LOCATION/ -r $svn_rev_start:$svn_rev_end update.ver`
 
 
 
@@ -17,11 +22,11 @@ else
 	if [[ "$experimental_update" ]]; then
 		#Do an experimental update!
 		Log "Preparing experimental update from r$svn_rev_start to r$svn_rev_end" 1
-		svn update $HOME/smartcoin/
+		svn update $CUR_LOCATION/
 	else
     		if [[ "$safe_update" ]]; then
      			Log "Preparing safe update from r$svn_rev_start to r$svn_rev_end" 1
-     			svn update $HOME/smartcoin/
+     			svn update $CUR_LOCATION/
    		 else
       			Log "There are new experimental updates, but they aren't proven safe yet. Not updating." 1
     		fi
@@ -32,7 +37,7 @@ else
 
  
 	#make sure that we backup the database before playing around with it!
-	cp $HOME/smartcoin/smartcoin.db $HOME/smartcoin/smarcoin.db.backup
+	cp $HOME/.smartcoin/smartcoin.db $HOME/.smartcoin/smarcoin.db.backup
 
 	echo ""
 	Log "Applying post update patches..." 1
@@ -45,15 +50,15 @@ else
 		case $i in
 	
 
-		#     270)
-		#        # Update schema going into r270
-		#         echo "Applying r$i patch..."
-		#        # Set up by default for stable updates!
-		#        Q="DELETE FROM settings WHERE data='dev_branch';"
-		#        RunSQL "$Q"
-		#        Q="INSERT INTO settings (data,value,description) VALUES ('dev_branch','stable','Development branch to follow (stable/experimental)');"
-		#        RunSQL "$Q"
-		#TODO: Uncomment all of this and update the revision number when ready to roll out experimental/stable updates        ;;
+		     300)
+		        # Update schema going into r300
+		         echo "Applying r$i patch..."
+		        # Set up by default for stable updates!
+		        Q="DELETE FROM settings WHERE data='dev_branch';"
+		        RunSQL "$Q"
+		        Q="INSERT INTO settings (data,value,description) VALUES ('dev_branch','stable','Development branch to follow (stable/experimental)');"
+		        RunSQL "$Q"
+			;;
     		*)
         		Log "No patches to apply to r$i" 1
         		;;
@@ -66,4 +71,5 @@ Log "Update task complete." 1
 echo ""
 echo "Update is now complete!  You should now restart smartcoin. Please hit any key to continue."
 read blah
+
 
