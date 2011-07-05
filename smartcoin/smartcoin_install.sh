8c43c806ac8fed88359334269b4c7f8dd55f6773
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-05 14:50:11 +0000

    make sure we copy over the database before actually using it

--- smartcoin/smartcoin_install.sh
+++ smartcoin/smartcoin_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUR_LOCATION="$( cd "$( dirname "$0" )" && pwd )"
 
-. $CUR_LOCATION/smartcoin_ops.sh
+
 
 #INSTALL_LOCATION=$1
 
@@ -86,6 +86,7 @@ echo ""
 
 
 # SQL DB calls start now, make sure we set the database
+. $CUR_LOCATION/smartcoin_ops.sh
 UseDB "smartcoin.db"
 
 # Set up the local machine
