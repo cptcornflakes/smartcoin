140774473c848399761d7af6c860945c631779db
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-16 14:04:57 +0000

    Fixes AMD/ATI SDK location detection in installer

--- smartcoin/smartcoin_install.sh
+++ smartcoin/smartcoin_install.sh
@@ -28,9 +28,18 @@ CheckIfAlreadyInstalled() {
 
 findAMDSDK()
 {
-	local location=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx(32|64)/lib/x86(_64)?$'
-`
-	echo "$location"
+	# local location=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx(32|64)/lib/x86(_64)?$'`
+	# Look for 64 bit version first
+	local location64=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx64/lib/x86_64?$'`
+	if [[ "$location64" != "" ]]; then
+		echo "$location64"
+		return
+	fi
+
+	# Look for 32 bit version
+	local location32=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx32/lib/x86?$'`
+	echo "$location32"
+
 }
 
 #################
