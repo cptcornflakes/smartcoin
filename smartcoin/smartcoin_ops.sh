3db2a228432521747f42fd10bf386f58b85b0c22
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-26 23:34:54 +0000

    trying something simple...

--- smartcoin/smartcoin_ops.sh
+++ smartcoin/smartcoin_ops.sh
@@ -153,7 +153,7 @@ startMiners() {
 		Launch $machine "screen -r $minerSession -X hardstatus on"
 		Launch $machine "screen -r $minerSession -X hardstatus alwayslastline"
 		local hard_status='%{= kG}[ %{G}%H %{g}][%= %{= kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u%{w}%?%+Lw%?%?%= %{g}][%{B} %m/%d/%y %{W}%c %{g}]'
-		Launch $machine "screen -r $minerSession -X hardstatus string '$hard_status'"
+		Launch $machine "screen -r $minerSession -X hardstatus string %w"
 
 		# Start all of the miner windows
 		for row in $FA; do
