577050f0699a73a7e12da30578c55e94ef5af30b
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-27 00:02:37 +0000

    .

--- branches/beta1/smartcoin/smartcoin_ops.sh
+++ branches/beta1/smartcoin/smartcoin_ops.sh
@@ -152,10 +152,10 @@ startMiners() {
 		Launch $machine "screen -r $minerSession -X chdir"
 		Launch $machine "screen -r $minerSession -X hardstatus on"
 		Launch $machine "screen -r $minerSession -X hardstatus alwayslastline"
-		local hard_status="%{= kG}[ %{G}%H %{g}][%= %{= kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%= %{g}][%{B} %m/%d/%y %{W}%c %{g}]"
+		local hard_status='%{= kG}[ %{G}%H %{g}][%= %{= kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%= %{g}][%{B} %m/%d/%y %{W}%c %{g}]'
 		# TODO: fix hardstatus line!!!
 		
-		Launch $machine "screen -r $minerSession -X hardstatus string '%{=\ kG}[\ %{G}%H\ %{g}][%=\ %{=\ kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%=\ %{g}][%{B}\ %m/%d/%y\ %{W}%c\ %{g}]'"
+		Launch $machine "screen -r $minerSession -X hardstatus string '$hard_status'"
 
 		# Start all of the miner windows
 		for row in $FA; do
