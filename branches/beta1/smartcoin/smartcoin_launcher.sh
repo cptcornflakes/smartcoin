56a366c433e45f54c455ae91c5dd1dd9cd5303b7
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-29 14:47:11 +0000

    - More cleanup of the AutoDetect routine - Phoenix path is now automagically added to LD_LIBRARY_PATH if needed! No more setting needed!

--- branches/beta1/smartcoin/smartcoin_launcher.sh
+++ branches/beta1/smartcoin/smartcoin_launcher.sh
@@ -52,6 +52,16 @@ minerLaunch=${minerLaunch//<#path#>/$minerPath}
 echo "LAUNCH: $minerLaunch"
 Log "Launching miner with launch string: $minerLaunch" 1
 
+case "$minerLaunch" in
+*phoenix.py*)
+	# Phoenix requires its path be in $LD_LIBRARY_PATH to launch from script
+	if [[ "$LD_LIBRARY_PATH" == *$minerPath* ]]; then
+		# It has not yet been added. Add the phoenixPath to LD_LIBRARY_PATH
+		export LD_LIBRARY_PATH=$minerPath:$LD_LIBRARY_PATH
+	fi
+	;;
+esac
+
 
 cd $minerPath && $minerLaunch 2>/dev/null
 
