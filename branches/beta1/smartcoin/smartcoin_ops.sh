51facefd360dd6a45b66e8ccaefb04fa86651a99
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-31 15:21:13 +0000

    I think I have the problem with remote tty allocation solved.  Call -n on the master connection and -t -t on the other connections

--- branches/beta1/smartcoin/smartcoin_ops.sh
+++ branches/beta1/smartcoin/smartcoin_ops.sh
@@ -892,18 +892,18 @@ Launch()
 		local port=$(Field 4 "$R")
 
 		# See if the persistent connection is available
-		res=$(ssh -t -t -p $port -i ~/.ssh/id_rsa.smartcoin -O check -S /tmp/smartcoin.ssh_connection.$machine $user@$server 2>&1 /dev/null)
+		res=$(ssh -p $port -i ~/.ssh/id_rsa.smartcoin -O check -S /tmp/smartcoin.ssh_connection.$machine $user@$server 2>&1 /dev/null)
 
 		if [[ $? -ne 0 ]]; then
 			# The connection does not exist!  Lets create it!
 			Log "Creating persistent ssh connection to machine $machine" 1
-			ssh -t -t -p $port -i ~/.ssh/id_rsa.smartcoin -o BatchMode=yes -NfM -S /tmp/smartcoin.ssh_connection.$machine $user@$server
+			ssh -n -p $port -i ~/.ssh/id_rsa.smartcoin -o BatchMode=yes -NfM -S /tmp/smartcoin.ssh_connection.$machine $user@$server
 		fi
  
 		if [[ -z "$no_block" ]]; then
-			res=$(eval "ssh -N -t -t -p $port -i ~/.ssh/id_rsa.smartcoin -o BatchMode=yes -S /tmp/smartcoin.ssh_connection.$machine $user@$server $cmd")
+			res=$(eval "ssh -t -t -p $port -i ~/.ssh/id_rsa.smartcoin -o BatchMode=yes -S /tmp/smartcoin.ssh_connection.$machine $user@$server $cmd")
 		else
-			eval "ssh -N -t -t -p $port -i ~/.ssh/id_rsa.smartcoin -o BatchMode=yes -S /tmp/smartcoin.ssh_connection.$machine $user@$server $cmd"
+			eval "ssh -t -t -p $port -i ~/.ssh/id_rsa.smartcoin -o BatchMode=yes -S /tmp/smartcoin.ssh_connection.$machine $user@$server $cmd"
 		fi
 	fi
 	if [[ -z "$no_block" ]]; then
