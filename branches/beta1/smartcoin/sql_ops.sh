f457b44f7e82b1571562f03b5adb954739cf4e83
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-31 14:45:20 +0000

    more RunSQL experiments

--- branches/beta1/smartcoin/sql_ops.sh
+++ branches/beta1/smartcoin/sql_ops.sh
@@ -17,13 +17,13 @@ RunSQL()
         Q="$*"
         if [[ -n "$Q" ]]; then
 		res=$(sqlite3 -noheader -separator "	" "$HOME"/.smartcoin/"$SQL_DB" "$Q;" 2> /dev/null)
-		err=$?
+		err="$?"
 
-		while [[ $err -ne 0 ]]; do
+		while [[ "$err" -ne 0 ]]; do
 			let i++
 			sleep 0.01
 			res=$(sqlite3 -noheader -separator "	" "$HOME"/.smartcoin/"$SQL_DB" "$Q;" 2> /dev/null)
-			err=$?
+			err="$?"
 			if [[ "$i" -gt 1000 ]]; then
 				Log "ERROR: SQL Query failed!"
 				Log "	error code: $?"
