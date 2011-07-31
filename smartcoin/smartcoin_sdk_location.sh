55174ebc77eeef67e803ca1edb6c0c20fd7927ba
jondecker76@gmail.com <jondecker76@gmail.com@a909248e-1086-ed9a-2b14-2260ea8a30f2>
2011-07-31 15:38:16 +0000

    Autodetection for remote machines should now work.  Now to test it for local machines!

--- smartcoin/smartcoin_sdk_location.sh
+++ smartcoin/smartcoin_sdk_location.sh
@@ -2,12 +2,13 @@
 
 # local location=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx(32|64)/lib/x86(_64)?$'`
 # Look for 64 bit version first
-location64=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx64/lib/x86_64?$'`
+location64=`find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx64/lib/x86_64?$' 2> /dev/null`
 if [[ "$location64" != "" ]]; then
 	echo "$location64"
 	exit
 fi
 
 # Look for 32 bit version
-location32=`sudo find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx32/lib/x86?$'`
+location32=`find / -type d -regextype posix-extended -iregex '.*/(AMD|ATI)-(APP|STREAM)-SDK-v[[:digit:].]+-lnx32/lib/x86?$' 2> /dev/null`
 echo "$location32"
+exit
